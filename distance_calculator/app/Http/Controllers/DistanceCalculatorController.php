bdebd2bfd23c64ebf135ca11a27fa5dee54ab7f0
nomsa <nomsam@smartz-solutions.com>
2021-07-10 13:30:29 +0200

    validation, testing and code usability

--- distance_calculator/app/Http/Controllers/DistanceCalculatorController.php
+++ distance_calculator/app/Http/Controllers/DistanceCalculatorController.php
@@ -4,138 +4,83 @@ namespace App\Http\Controllers;
 
 use http\Client\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class DistanceCalculatorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-
-    }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function create()
-    {
-        //
-    }
+    public function processData(Request $request){
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
-    public function get_distance(Request $request){
-        $accepted_params = ["meters","yards","unit"];
         $arrData = $request->all();
-        try {
-            if (count($arrData) == 3) {
-                $requester_unit = "";
-                $distance_all_accepted = ["meters","yards"];
-                $distance = [];
-                $converted_numbers = [];
-
-                if(array_key_exists('unit', $arrData)){
+        $rules = [
+            'first_distance' => 'required',
+            'second_distance'=>'required',
+            'unit'=>'required'
+        ];
+        $validator = Validator::make($arrData, $rules);
+        if ($validator->passes()) {
+            try {
+                if (count($arrData) == 3) {
+                    $distance_all_accepted = ["meters","yards"];
+                    $distance = [];
                     $requester_unit = $arrData['unit'];
                     unset($arrData['unit']);
-                    if(!in_array($requester_unit,$distance_all_accepted)){
-                        return response()->json(["error"=>"requester unit incorrect"],500);
-                    }
-                }
-                else{
-                    return response()->json(["error"=>"unit key incorrect"],500);
-                }
 
-                foreach ($arrData as  $arr_value){
-                    foreach ($arr_value as $unit => $value){
-                        if(in_array($unit,$distance_all_accepted)){
-                            if(is_numeric($value)){
-                                array_push($distance,["distance"=>$value,"unit"=>$unit]);
-                            }
-                            else{
-                                return response()->json(["error"=>"unit value incorrect"],500);
+                    foreach ($arrData as $arr_value){
+                        if(is_array($arr_value)){
+                            foreach ($arr_value as $unit => $value){
+                                if(!in_array($unit,$distance_all_accepted)){
+                                    return response()->json(["error"=>"unit value incorrect"],400);
+                                }
+                                else{
+                                    if(is_numeric($value)){
+                                        array_push($distance,["distance"=>$value,"unit"=>$unit]);
+                                    }
+                                    else{
+                                        return response()->json(["error"=>"only numbers are permitted"],400);
+                                    }
+                                }
                             }
                         }
                         else{
-                            return response()->json(["error"=>"unit specified incorrect"],500);
+                            return response()->json(["error"=>"unit value incorrect"],400);
                         }
                     }
+                    $converted_distance = self::get_distance($requester_unit,$distance);
+                    return response()->json($converted_distance." ".$requester_unit,200);
                 }
-                foreach ($distance as $value) {
-                    if($requester_unit === "meters"){
-                            $converted_numbers[] = ($value['unit'] === "meters")? $value['distance'] : ($value['distance'] * 0.9144);
-                    }
-                    else{
-                        $converted_numbers[] = ($value['unit'] === "meters")? ($value['distance'] * 1.094) : $value['distance'];
-                    }
+                else {
+                    return response()->json(["error" => "Only 3 arguments allowed and keys needs to match"]);
                 }
-                $final_distance_converted = round(array_sum($converted_numbers),2);
-                return response()->json($final_distance_converted." ".$requester_unit,200);
             }
-            else {
-                return response()->json(["error" => "Only 3 arguments allowed and keys needs to match"]);
+            catch (\Exception $e) {
+                return response()->json(["error" => $e->getMessage()],500);
             }
         }
-        catch (\Exception $e) {
-            return response()->json(["error" => $e->getMessage()]);
+        else{
+            return response()->json($validator->errors(), 400);
         }
+    }
+
+    /**
+     * @param $unit
+     * @param $data
+     * @return float
+     */
+    public static function get_distance($unit,$data){
+        $converted_numbers = [];
+        foreach ($data as $value) {
+            if($unit === "meters"){
+                $converted_numbers[] = ($value['unit'] === "meters")? $value['distance'] : ($value['distance'] * 0.9144);
+            }
+            else{
+                $converted_numbers[] = ($value['unit'] === "meters")? ($value['distance'] * 1.094) : $value['distance'];
+            }
+        }
+        return round(array_sum($converted_numbers),2);
     }
 }
--- distance_calculator/routes/api.php
+++ distance_calculator/routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::any('calculate_distance', [DistanceCalculatorController::class, 'get_distance']);
+Route::any('calculate_distance', [DistanceCalculatorController::class, 'processData']);
